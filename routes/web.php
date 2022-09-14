b6a071c5d9b36ab9a31fc68daf23d2391fc0f056
Takayuki925Mago <takayuki.honda02@magoshichi.com>
2022-09-14 16:10:15 +0900

    [Add] Add login system Adding system of recognization user by Jetstreem

--- routes/web.php
+++ routes/web.php
@@ -13,7 +13,21 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('', 'App\Http\Controllers\JbeautyMagoshichi@teaser_page');
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/teaser', 'App\Http\Controllers\JbeautyMagoshichi@teaser_page');
 Route::get('/index', 'App\Http\Controllers\JbeautyMagoshichi@main_page');
 Route::get('/member', 'App\Http\Controllers\JbeautyMagoshichi@member_page');
 Route::get('/article', 'App\Http\Controllers\JbeautyMagoshichi@article_page');
+
+Route::middleware([
+    'auth:sanctum',
+    config('jetstream.auth_session'),
+    'verified'
+])->group(function () {
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
+});
