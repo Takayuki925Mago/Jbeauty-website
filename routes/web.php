03547187e1f763ee44b0410c42f3d72a950d2696
Takayuki925Mago <takayuki.honda02@magoshichi.com>
2022-10-18 15:28:53 +0900

    [Create] login function

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,21 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('/welcome', function () {
+    return view('welcome');
+});
+
+Route::middleware([
+    'auth:sanctum',
+    config('jetstream.auth_session'),
+    'verified'
+])->group(function () {
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
+});
+
+
 Route::get('/floor-guide', function() {
     return view('floor_guide');
 });
